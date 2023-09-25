0ae6c8726a52c7b5253a2b0dfb27694dbda74a32
gfuselier <gisellefuselier@gmail.com>
2023-09-25 15:03:35 -0500

    started organizing structure

---
+++ script.js
@@ -0,0 +1,16 @@
+//click a button to hide the start screen
+//select currentQuestion header and update content with current question
+//select ul with id of choices
+//create 4 buttons with textcontent and value of the answer choices
+
+var choices = ["apple", "kiwi", "banana", "peach"]
+for (let i = 0; i < choices.length; index++) {
+    //create a button for the choice
+    //add content
+    //append to the button container
+
+    //then clear the buttons before the next question
+}
+
+//big hint = custom data attributes activity(#19) for adding the event listener to the container
+//and then only care if element.matches("button")
