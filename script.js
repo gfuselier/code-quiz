3a0c51f9e5136a8ee809aacaa7683c3f7c92173c
gfuselier <gisellefuselier@gmail.com>
2023-09-26 18:00:20 -0500

    move forward, checkAns, timer, local storage

--- script.js
+++ script.js
@@ -7,98 +7,118 @@ var currentQuestion = document.getElementById("current-question")
 var answerChoices = document.getElementById("answer-choices")
 var timerElement = document.getElementById("timer-count")
 var timerCount = 100
+var level = 0
+var timer; 
+var scoresArr = JSON.parse(localStorage.getItem("highScores")) || []
 
-var question1 = {
-    questionText: "What color is the sky?",
-    choices:["blue","red","green","purple"],
-    correctChoice: "blue"
-}
-var question2 = {
-    questionText: "Who is Ernie's best friend?",
-    choices:["Big-Bird","Elmo","Bert","Oscar"],
-    correctChoice: "Bert"
-}
-var question3 = {
-    questionText: "What is the highest selling Halloween Candy each year?",
-    choices:["Snickers","Reese's","Skittles","Twinkies"],
-    correctChoice: "Reese's"
-}
-var question4 = {
-    questionText: "Question 4",
-    choices:["A","B","C","D"],
-    correctChoice: "D"
-}
-var question5 = {
-    questionText: "Question 5",
-    choices:["1","2","3","4"],
-    correctChoice: "1"
-}
+
+
 var allQuestions = [
-    question1, question2, question3, question4, question5
+    {
+        questionText: "What color is the sky?",
+        choices: ["blue", "red", "green", "purple"],
+        correctChoice: "blue"
+    }, {
+        questionText: "Who is Ernie's best friend?",
+        choices: ["Big-Bird", "Elmo", "Bert", "Oscar"],
+        correctChoice: "Bert"
+    }, {
+        questionText: "What is the highest selling Halloween Candy each year?",
+        choices: ["Snickers", "Reese's", "Skittles", "Twinkies"],
+        correctChoice: "Reese's"
+    }, {
+        questionText: "Question 4",
+        choices: ["A", "B", "C", "D"],
+        correctChoice: "D"
+    }, {
+        questionText: "Question 5",
+        choices: ["1", "2", "3", "4"],
+        correctChoice: "1"
+    }
 ]
 
 
 //click a button to hide the start screen
 function startQuiz() {
-    // startTimer()
+    startTimer();
     startingPage.style.display = "none";
     questionPage.style.display = "block";
 
-    function moveForward() {
+    moveForward();
+
+}
+
+function moveForward() {
     //select currentQuestion header and update content with current question
     //select ul with id of choices
     //create 4 buttons with textcontent and value?? of the answer choices
-    for (var i = 0; i < allQuestions[i].choices.length; i++) {
-        currentQuestion.textContent = allQuestions[i].questionText;
+    currentQuestion.textContent = allQuestions[level].questionText;
+    // currentQuestion.textContent = allQuestions[i + 1].questionText;
+    answerChoices.innerHTML = ""
+    for (var i = 0; i < 4; i++) {
         var ansButton = document.createElement("button");
-        ansButton.textContent = allQuestions[i].choices[i];
+        ansButton.textContent = allQuestions[level].choices[i]; 
+        ansButton.onclick = checkAns
         answerChoices.append(ansButton);
-        
+
     };
-    //clear the buttons before the next question??
     
-    questionPage.addEventListener("click", function(event) {
-        var element = event.target;
 
-        if (element.matches("button")) {
-            console.log(element.textContent);
-            if (element.textContent === allQuestions[0].correctChoice) {
-                console.log("correct!");
-            }
-            //get value of the button they clicked
-            //if (value === true) { go to next question}
-            //else {subtract 10 from the timer, go to next question}
-        }
-
-    })
-     
-    }
     
 }
 
-// function startTimer() {
-//     // Sets timer
-//     timer = setInterval(function() {
-//       timerCount--;
-//       timerElement.textContent = timerCount;
-//       if (timerCount >= 0) {
-//         // Tests if win condition is met
-//         if (isWin && timerCount > 0) {
-//           // Clears interval and stops timer
-//           clearInterval(timer);
-//           winGame();
-//         }
-//       }
-//       // Tests if time has run out
-//       if (timerCount === 0) {
-//         // Clears interval
-//         clearInterval(timer);
-//         loseGame();
-//       }
-//     }, 1000);
-//   }
-startButton.addEventListener("click", startQuiz)
+ function checkAns(event) {
+    var element = event.target;
+    console.log(element);
+    console.log(this);
+    if (element.matches("button")) {
+        console.log(element.textContent);
+        if (element.textContent === allQuestions[level].correctChoice) {
+            console.log("correct!");
+            
+        } else {
+            console.log("wrong!");
+            timerCount -= 10;
+            
+        }
 
+    }
+ level++
+ if (level >= allQuestions.length) {
+    console.log("Game Over")
+    gameOver();
+ } else {moveForward()}
+}
+
+function gameOver() {
+    clearInterval(timer);
+    timerElement.textContent = timerCount;
+    questionPage.style.display = "none";
+    endPage.style.display = "block";
+    
+}
+
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      // Tests if time has run out
+      if (timerCount <= 0) {
+        gameOver();
+      }
+    }, 1000);
+  }
+startButton.addEventListener("click", startQuiz)
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault();
+    var playerObj = {
+        initials: document.getElementById("initials").value,
+        score: timerCount
+    }
+    scoresArr.push(playerObj);
+    localStorage.setItem("highScores",JSON.stringify(scoresArr));
+})
 
 
 //What's left? 
