22941aaeb7007fdedaf774f5942d7dc718783578
gfuselier <gisellefuselier@gmail.com>
2023-09-25 21:45:13 -0500

    start button event listener

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-var startButton = document.getElementById
+var startButton = document.getElementById("start-button")
+var startingPage = document.getElementById("starting-page")
 
 //click a button to hide the start screen
+function startQuiz() {
+    startingPage.style.display = "none"
+}
+startButton.addEventListener("click", startQuiz)
 //select currentQuestion header and update content with current question
 //select ul with id of choices
 //create 4 buttons with textcontent and value of the answer choices
 
-var choices = ["apple", "kiwi", "banana", "peach"]
-for (let i = 0; i < choices.length; index++) {
+// var choices = ["apple", "kiwi", "banana", "peach"]
+// for (let i = 0; i < choices.length; index++) {
     //create a button for the choice
     //add content
     //append to the button container
 
     //then clear the buttons before the next question
-}
+// }
 
 //big hint = custom data attributes activity(#19) for adding the event listener to the container
 //and then only care if element.matches("button")
